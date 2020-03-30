f1eed802c8ff659650395e18f50287baeba343ef
Evilsir <i2cherry941219@gmail.com>
2020-03-30 17:10:54 +0800

    配置打包编译前清理dist目录

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const base = require('./webpack.config.base.js');
 
 module.exports = merge(base, {
@@ -8,6 +9,7 @@ module.exports = merge(base, {
     filename: 'js/[name].[contenthash:8].js'
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'public/index.html'
