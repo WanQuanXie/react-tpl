ac8f5a70e8a0adb96e320789ab87315012659013
Evilsir <i2cherry941219@gmail.com>
2020-03-30 17:06:53 +0800

    配置自动编译HTML并引入js文件

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,6 +1,16 @@
 const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const base = require('./webpack.config.base.js');
 
 module.exports = merge(base, {
-  mode: 'production'
+  mode: 'production',
+  output: {
+    filename: 'js/[name].[contenthash:8].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.html'
+    })
+  ]
 });
