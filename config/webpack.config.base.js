57fd785105bdb40c31b21dd4487f1585322b28b6
Evilsir <i2cherry941219@gmail.com>
2020-04-19 02:52:42 +0800

    优化 resolve.alias 配置; 优化 resolve.extensions 配置

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,4 +1,5 @@
 const threadLoader = require('thread-loader');
+const paths = require('./paths');
 
 // thread-loader 预热
 threadLoader.warmup({}, ['babel-loader']);
@@ -8,6 +9,24 @@ module.exports = {
     app: './src/index.js',
     // 配置 code spliting，将这些公共的复用代码最终抽取成一个 chunk，单独打包出来
     framework: ['react', 'react-dom'],
+  },
+  resolve: {
+    // import导入时省略后缀
+    // 注意：尽可能的减少后缀尝试的可能性
+    extensions: ['.js', '.jsx', '.less', '.css', '.json'],
+    // 优化模块查找路径
+    modules: [
+      // 设置模块导入规则，import/require时会直接在这些目录找文件
+      // 可以指明存放第三方模块的绝对路径，以减少寻找，
+      // 默认 node_modules
+      paths.SOURCE_DIR,
+      paths.NODE_MODULES_DIR,
+    ],
+    // import 导入时别名，减少耗时的递归解析操作
+    alias: {
+      '@': paths.SOURCE_DIR,
+      '@components': paths.COMPONENTS,
+    },
   },
   module: {
     rules: [
