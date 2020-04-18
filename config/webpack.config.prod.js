79a1acc4ecb4ba0debefe69d2548b12db94537b8
Evilsir <i2cherry941219@gmail.com>
2020-04-19 02:43:29 +0800

    优化 loader 配置

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -45,13 +45,24 @@ const prodWebpackConfig = merge(base, {
   },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-      },
+      // 针对业务代码中的 less 处理
       {
         test: /\.less$/,
+        exclude: /node_modules/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'less-loader'],
+      },
+      // 专门针对 node_module 中的 less 处理
+      {
+        test: /\.less$/,
+        include: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'less-loader'],
+      },
+      // 为避免有些第三方库提供的 CSS 没有做浏览器兼容性处理，在加载 node_moduels 中的 CSS 之前还要使用 postcss-loader 再统一处理一遍，
+      // 以确保所有进入生产环境的 CSS 都经过了相应的浏览器兼容性处理
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
     ],
   },
